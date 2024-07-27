a07472e7c14ecca695f237d47aa7cd29982c6cf4
Declan Munene <munene.declan1@gmail.com>
2024-07-27 18:23:31 +0300

    Hot fix!

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -30,7 +30,7 @@ CREATE TABLE Orders (
 
 CREATE TABLE Order_Details (
 	order_detail_id INT PRIMARY KEY,
-	FOREIGN KEY (order_id) REFERENCES Orders (order_id),
+	FOREIGN KEY (order_id) REFERENCES Orders(order_id),
 	FOREIGN KEY (book_id) REFERENCES Books(book_id),
 	quantity DOUBLE
 );
